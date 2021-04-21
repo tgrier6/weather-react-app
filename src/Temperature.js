1fb72d459320aedd2a4640c81917e90b6e2d57e8
tgrier6 <tiaragrier2@gmail.com>
2021-04-21 15:36:29 -0400

    index.js Reset

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,49 +1,5 @@
 import React from "react";
-import "./Temperature.css";
-import DateTime from "./DateTime";
-import Humidity from "./Humidity";
-import Wind from "./Wind";
-import Description from "./Description";
 
 export default function Temperature() {
-  return (
-    <div className="card mb-3" id="top-display">
-      <div className="row g-0" id="td-weather">
-        <div className="col-md-6">
-          <img
-            src="http://openweathermap.org/img/wn/03d@2x.png"
-            className="Sunshine"
-            alt=""
-            id="icon"
-            width="300px"
-          />
-        </div>
-
-        <div className="col-md-6">
-          <div className="card-body">
-            <h3 className="card-title" id="city">
-              Atlanta, GA
-            </h3>
-            <ul>
-              <li>
-                <Temperature />
-              </li>
-              <li>
-                <DateTime />
-              </li>
-              <li>
-                <Humidity />
-              </li>
-              <li>
-                <Wind />
-              </li>
-              <li>
-                <Description />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>81˚F</div>;
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Temperature from "./Temperature";
+import DateTime from "./DateTime";
+import Humidity from "./Humidity";
+import Wind from "./Wind";
+import Description from "./Description";
 
 import "./App.css";
 
@@ -28,7 +32,45 @@ function App() {
             </button>
           </form>
         </form>
-        <Temperature />
+
+        <div className="card mb-3" id="top-display">
+          <div className="row g-0" id="td-weather">
+            <div className="col-md-6">
+              <img
+                src="http://openweathermap.org/img/wn/03d@2x.png"
+                className="Sunshine"
+                alt=""
+                id="icon"
+                width="300px"
+              />
+            </div>
+
+            <div className="col-md-6">
+              <div className="card-body">
+                <h3 className="card-title" id="city">
+                  Atlanta, GA
+                </h3>
+                <ul>
+                  <li>
+                    <Temperature />
+                  </li>
+                  <li>
+                    <DateTime />
+                  </li>
+                  <li>
+                    <Humidity />
+                  </li>
+                  <li>
+                    <Wind />
+                  </li>
+                  <li>
+                    <Description />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="card-group">
