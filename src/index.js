4574f97fdf891dd4c3c4ae479bae69bf53308bbc
tgrier6 <tiaragrier2@gmail.com>
2021-04-21 15:20:04 -0400

    Moving top card into Temperature.js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Temperature from "./Temperature";
-import DateTime from "./DateTime";
-import Humidity from "./Humidity";
-import Wind from "./Wind";
-import Description from "./Description";
 
 import "./App.css";
 
@@ -33,45 +29,7 @@ function App() {
             </button>
           </form>
         </form>
-
-        <div className="card mb-3" id="top-display">
-          <div className="row g-0" id="td-weather">
-            <div className="col-md-6">
-              <img
-                src="http://openweathermap.org/img/wn/03d@2x.png"
-                className="Sunshine"
-                alt=""
-                id="icon"
-                width="300px"
-              />
-            </div>
-
-            <div className="col-md-6">
-              <div className="card-body">
-                <h3 className="card-title" id="city">
-                  Atlanta, GA
-                </h3>
-                <ul>
-                  <li>
-                    <Temperature />
-                  </li>
-                  <li>
-                    <DateTime />
-                  </li>
-                  <li>
-                    <Humidity />
-                  </li>
-                  <li>
-                    <Wind />
-                  </li>
-                  <li>
-                    <Description />
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Temperature />
       </div>
 
       <div className="card-group">
